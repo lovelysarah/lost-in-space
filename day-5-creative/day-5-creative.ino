c5cb8210298b866f369c430eb8340d8d4b5eb12e
Sarah R <robichaudsarah@outlook.com>
2022-12-25 12:08:05 -0400

    :sparkles: Personal project #1

---
+++ day-5-creative/day-5-creative.ino
@@ -0,0 +1,53 @@
+int switch1 = 2;
+
+int active;
+int previous;
+int minPin = 11;
+int maxPin = 13;
+
+bool powered = false;
+
+void setup() {
+  // Loop through pins from min to max.
+  for(int i = minPin; i > maxPin+1; i++){
+    pinMode(i, OUTPUT);
+  }
+  pinMode(switch1, INPUT);
+}
+
+
+void wave() {
+  if(previous) {
+    digitalWrite(previous, LOW);
+  }
+  digitalWrite(active, HIGH);
+}
+
+void turnOff(){
+  // Loop through pins from min to max.
+  for(int i = minPin; i < maxPin + 1; i++){
+    digitalWrite(i, LOW);
+  }
+  powered = false;
+}
+
+void loop() {
+  // If switch input reads a high voltage, take the happy path.
+  if(digitalRead(switch1) == HIGH) { 
+
+    powered = true;
+    delay(750);
+    
+    // If active isn't set and or it has reached the end, set it to first pin.
+    if(!active || active == maxPin+1){
+      active = minPin;
+    };
+
+    wave();
+    previous = active;
+    active++;
+    
+  } else if (powered) {
+    turnOff();
+  }
+}
